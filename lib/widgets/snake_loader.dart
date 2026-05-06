f7fc56e7d495f89a03c00e5648502c8cf2f0f892
Katarzyna Hajduk <ktrina010@gmail.com>
2026-05-06 10:41:21 +0200

    Fix

--- lib/widgets/snake_loader.dart
+++ lib/widgets/snake_loader.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class SnakeLoader extends StatelessWidget {
   final double progress;
-
   const SnakeLoader({super.key, required this.progress});
 
   @override
